c1b3c044469164822f3eb34b9b161c8088a82a39
Serghei MIHAI <smihai@entrouvert.com>
2015-03-11 16:58:35 +0100

    footer links localized (#6707)

--- ckanext/ozwillo_theme/plugin.py
+++ ckanext/ozwillo_theme/plugin.py
@@ -1,8 +1,36 @@
+import json
+import os
+import requests
+import xml.etree.ElementTree as ET
+from slugify import slugify
+
 from pylons import config as pconfig
 
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckan.lib.app_globals import set_global
+
+def footer_links():
+    url = 'https://www.ozwillo.com/footer.xml'
+    langs = {}
+
+    response = requests.get(url)
+    menuset = ET.fromstring(response.text.encode('utf-8'))
+
+    items = ('News', 'Discovering', 'Co-construct', 'Let\'s go', 'Contact',
+             'Projects', 'Project team', 'Ozwillo', 'User guide', 'Developers',
+             'Legal Notices', 'General terms of use')
+
+
+    for menu in menuset.findall('menu'):
+        locale = menu.find('locale').text
+        c = 0
+        langs[locale] = {}
+        for item in menu.findall('item'):
+            if 'href' in item.attrib:
+                langs[locale][slugify(items[c])] = item.get('href')
+                c += 1
+    return langs
 
 
 class OzwilloThemePlugin(plugins.SingletonPlugin):
@@ -13,6 +41,8 @@ class OzwilloThemePlugin(plugins.SingletonPlugin):
                    pconfig.get('%s.ozwillo_url' % __name__))
         set_global('ckan.ozwillo_portal_url',
                    pconfig.get('%s.ozwillo_portal_url' % __name__))
+
+        set_global('ckan.footer_links', footer_links())
 
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
