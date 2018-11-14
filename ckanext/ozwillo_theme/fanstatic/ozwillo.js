31af0c2059c69ca6ef0fced85e405ff1f1bfd525
Benoit Orihuela <borihuela@ozwillo.org>
2018-11-14 09:52:49 +0100

    SICTIAM theme v1

--- ckanext/ozwillo_theme/fanstatic/ozwillo.js
+++ ckanext/ozwillo_theme/fanstatic/ozwillo.js
@@ -1,12 +1,12 @@
 
 $(document).ready(function() {	
-
+	
 	// COUNTER ANIMATION
 	$('.numbers').each(function () {
 	    $(this).prop('Counter',0).animate({
 	        Counter: $(this).text()
 	    }, {
-	        duration: 4000,
+	        duration: 5000,
 	        easing: 'swing',
 	        step: function (now) {
 	            $(this).text(Math.ceil(now));
