2de3e68807e29819bb54869f914884ebe1472241
Benoit Orihuela <borihuela@ozwillo.org>
2018-11-23 17:59:36 +0100

    SICTIAM theme - final version

--- ckanext/ozwillo_theme/fanstatic/ozwillo.js
+++ ckanext/ozwillo_theme/fanstatic/ozwillo.js
@@ -2,16 +2,37 @@
 $(document).ready(function() {	
 	
 	// COUNTER ANIMATION
-	$('.numbers').each(function () {
-	    $(this).prop('Counter',0).animate({
-	        Counter: $(this).text()
-	    }, {
-	        duration: 5000,
-	        easing: 'swing',
-	        step: function (now) {
-	            $(this).text(Math.ceil(now));
-	        }
-	    });
-	});
+	if ( $( ".numbers" ).length ) {
+		$('.numbers').each(function () {
+		    $(this).prop('Counter',0).animate({
+		        Counter: $(this).text()
+		    }, {
+		        duration: 5000,
+		        easing: 'swing',
+		        step: function (now) {
+		            $(this).text(Math.ceil(now));
+		        }
+		    });
+		});
+	}
+
+	// STICKY MENU
+	// $(window).scroll(function () {
+	//       //if you hard code, then use console
+	//       //.log to determine when you want the 
+	//       //nav bar to stick.  
+	//       //console.log($(window).scrollTop())
+	//     if ($(window).scrollTop() > 120) {
+	//       $('#t3-mainnav').addClass('navbar-fixed-top');
+	//       $("#content").css("padding-top","60px");
+	//       $(".homepage").css("padding-top","60px");
+	      
+	//     }
+	//     if ($(window).scrollTop() < 121) {
+	//       $('#t3-mainnav').removeClass('navbar-fixed-top');
+	//       $("#content").css("padding-top","10px");
+	//       // $(".homepage").css("padding-top","10px");
+	//     }
+	//   });
 
 });
--- ckanext/ozwillo_theme/fanstatic/particles.js
+++ ckanext/ozwillo_theme/fanstatic/particles.js
@@ -33,7 +33,7 @@ var pJS = function(tag_id, params){
         type: 'circle',
         stroke: {
           width: 0,
-          color: '#ff0000'
+          color: '#fff'
         },
         polygon: {
           nb_sides: 5
