55665e7252beef805a10545b32a9d5705a174ed5
Benoit Orihuela <borihuela@ozwillo.org>
2018-11-27 08:53:09 +0100

    SICTIAM theme - final version #2

--- ckanext/ozwillo_theme/fanstatic/particles.js
+++ ckanext/ozwillo_theme/fanstatic/particles.js
@@ -33,7 +33,7 @@ var pJS = function(tag_id, params){
         type: 'circle',
         stroke: {
           width: 0,
-          color: '#fff'
+          color: '#ff0000'
         },
         polygon: {
           nb_sides: 5
