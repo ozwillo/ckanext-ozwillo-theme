4d6424ba93c28896f860a7f4316a2920d8c4c812
tanvirchahal <tnvr009@gmail.com>
2019-09-05 22:32:15 +0530

    fixed checkboxes bug

--- create.py
+++ create.py
@@ -1,10 +1,11 @@
 import requests
 
 API='24365b05-a5f2-44ae-a6c6-4379bfd44334'
-URL= 'http://ckan-dev:5000/api/3/action/organization_create'
+URL= 'http://ckan-dev:5000/api/3/action/group_create'
 
 data = {
-    'name': 'test_org'
+	'name': 'ddd',
+	'state':'active'
 }
 
 headers = {
