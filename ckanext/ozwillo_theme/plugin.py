323c80a9b4ae05b3ace0f4cdc9129fe2da99d33b
Marc Dutoo <marc.dutoo@gmail.com>
2022-04-15 10:39:56 +0200

    completed FDR theme structure : orgs & showcases : take 4 at random, display description (but not datasets for orgs, rather manually in the description), text runs around card icon retrieve org fields (for description) completed non territory org filter hide group second line, tagline

--- ckanext/ozwillo_theme/plugin.py
+++ ckanext/ozwillo_theme/plugin.py
@@ -7,6 +7,7 @@ from ckan.lib.plugins import DefaultTranslation
 import ckan.logic as logic
 import ckan.lib.helpers as h
 from datetime import datetime
+from random import randrange
 
 
 class OzwilloThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
@@ -41,9 +42,9 @@ class OzwilloThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
             'ozwillo_theme_spatial_installed': lambda: config.get('ckanext.ozwillo_theme.spatial_installed', 'False'),
             'ozwillo_theme_osmnames_key': lambda: config.get('ckanext.ozwillo_theme.osmnames_key', ''),
             # [FDR]
-            'ozwillo_theme_get_project_orgs': lambda: logic.get_action('organization_autocomplete')({}, {"q": "usage-"}),
-            'ozwillo_theme_get_non_project_organizations': lambda: logic.get_action('organization_list')({}, {"limit": 4, 'all_fields': True, 'sort': 'name asc'}),
-            'ozwillo_theme_get_showcases': lambda: logic.get_action('ckanext_showcase_list')({}, {})
+            'ozwillo_theme_get_project_orgs': ozwillo_theme_get_project_orgs,
+            'ozwillo_theme_get_showcases': ozwillo_theme_get_showcases,
+            'ozwillo_theme_get_territory_organizations': ozwillo_theme_get_territory_organizations
         }
 
 
@@ -70,3 +71,30 @@ def ozwillo_theme_get_map(view_id, resource_id, package_id):
     package = logic.get_action('package_show')({}, {'id': package_id})
 
     return h.rendered_resource_view(resource_view, resource, package, True)
+
+def ozwillo_theme_get_project_orgs():
+    usage_prefixed_short_orgs = logic.get_action('organization_autocomplete')({}, {"q": "usage-"})
+    usage_prefixed_orgs = [logic.get_action('organization_show')({}, {"id": org['name']}) for org in usage_prefixed_short_orgs]
+    return usage_prefixed_orgs
+
+def ozwillo_theme_get_showcases():
+    showcases = logic.get_action('ckanext_showcase_list')({}, {})
+    # take 4 at random :
+    random_start_index = randrange(len(showcases))
+    return showcases[random_start_index:random_start_index + 4]
+
+def ozwillo_theme_get_territory_organizations():
+    all_org_names = logic.get_action('organization_list')({}, {"limit": 1000, 'sort': 'name asc'})
+    # take 50 at random :
+    random_start_index = randrange(len(all_org_names))
+    org_names = all_org_names[random_start_index:random_start_index + 50]
+
+    # filter :
+    usage_prefixed_org_names = logic.get_action('organization_autocomplete')({}, {"q": "usage-"})
+    helper_org_names = ['france-data-reseau','federation-nationale-des-collectivites-concedantes-et-regies', 'datactivist' 'altereo', 'darkskylab']
+    non_territory_org_names = helper_org_names + usage_prefixed_org_names
+    territory_org_names = [org_name for org_name in org_names if org_name not in non_territory_org_names][0:4]
+
+    # get :
+    territory_orgs = [logic.get_action('organization_show')({}, {"id": org_name}) for org_name in territory_org_names]
+    return territory_orgs
